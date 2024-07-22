cc8d203055fe053cfd200e1cb8ceffe78ac36342
oliver <oliverbcontact@gmail.com>
2024-07-21 22:04:12 -0400

    added font set and fixed some operations

--- js/app.js
+++ js/app.js
@@ -4,9 +4,12 @@ const INIT_HERTZ = 60; // starting hertz for timer registers
 const HEIGHT = 32; // display resolution height
 const WIDTH = 64; // display resolution width
 const PROGRAM_START = 0x200;
-const MEMORY_LOCATIONS = 0x1000;
+const FONTS_START = 0x50;
+const FONT_LEN = 5; // number of bytes for a font character
+const MEMORY_SIZE = 0x1000;
 const BEEP_SOUND_FILE = "./audio/beep-06.mp3"
 const SPRITE_WIDTH = 8;
+
 
 const INPUT_MAP = {
   "1": 1, "2": 2, "3": 4, "4": 0xC,
@@ -14,6 +17,25 @@ const INPUT_MAP = {
   'a': 7, 's': 8, 'd': 9, 'f': 0xE,
   'z': 0xA, 'x': 0, 'c': 0xB, 'v': 0xF
 }
+
+const FONT_SET = [
+  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+  0x20, 0x60, 0x20, 0x20, 0x70, // 1
+  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+]
 
 const sprite_addr = [];
 
@@ -42,12 +64,20 @@ function waitForKeyPress() {
   );
 }
 
+const ON = 0xFFFFFFFF;
+const OFF = 0;
+
 class CPU {
   constructor(canvas) {
     this.canvas = canvas
     this.ctx = this.canvas.getContext("2d");
-    this.clear_display()
-    this.memory = new Uint8Array(MEMORY_LOCATIONS);
+    this.memory = new Uint8Array(MEMORY_SIZE);
+    // load fonts into memory
+    for (let i = 0; i < FONT_SET.length; i++) {
+      this.memory[FONTS_START + i] = FONT_SET[i];
+    }
+
+
     this.stack = new Uint16Array(16);
 
     this.registers = new Uint8Array(NUM_REGISTERS);
@@ -57,10 +87,15 @@ class CPU {
     this.delay_timer = INIT_HERTZ;
     this.sound_timer = INIT_HERTZ;
     this.interval = null;
+    this.display = new Uint32Array(WIDTH * HEIGHT);
+    this.video = new Uint8ClampedArray(WIDTH * HEIGHT);
 
   }
 
   clear_display() {
+    for (let i = 0; i < this.display.length; i++)
+      this.display[i] = 0;
+
     this.ctx.fillStyle = "black";
     this.ctx.fillRect(0, 0, WIDTH, HEIGHT);
   }
@@ -79,18 +114,21 @@ class CPU {
       this.load_rom(rom)
       this.interval = setInterval(async () => {
         await this.cycle()
-      }, 10);
+        this.update()
+      }, Math.floor(1000 / 60));
       resolve();
     })
 
 
   }
 
+  update() {
+  }
+
   async cycle() {
     return new Promise(async (resolve) => {
-      let opcode, addr, x, y, c, ans, msb, lsb, n, key;
+      let opcode, addr, x, y, c, ans, msb, lsb, height, key;
       opcode = this.memory[this.pc] << 8 | this.memory[this.pc + 1];
-      console.log(opcode)
       switch (opcode & 0xF000) {
         case 0x0000:
           switch (opcode & 0x0F00) {
@@ -101,19 +139,16 @@ class CPU {
                   this.pc += 2;
                   break
                 case 0: // return from subroutine
-                  addr = this.stack[this.sp]; // pop from stack
-                  this.sp--;
+                  addr = this.stack[--this.sp]; // pop from stack
                   this.pc = addr;
                   break
                 default:
                   console.log(`unrecognized opcode`)
-                  alert("unrecognized opcode")
                   break;
               }
               break;
             default:
               console.log(`unrecognized opcode: ${opcode}`);
-              alert("unrecognized opcode")
               break
           }
           break;
@@ -122,8 +157,10 @@ class CPU {
           this.pc = addr;
           break;
         case 0x2000: // call subroutine at address
+          this.pc += 2;
           addr = opcode & 0x0FFF;
-          this.stack[this.sp++] = this.pc + 2; // push return address to stack
+          this.stack[this.sp] = this.pc; // push return address to stack
+          ++this.sp;
           this.pc = addr;
           break;
         case 0x3000: // skip next instruction if Vx = NN
@@ -183,13 +220,13 @@ class CPU {
               x = (opcode & 0x0F00) >> 8;
               y = (opcode & 0x00F0) >> 4;
               ans = Number(this.registers[x]) + Number(this.registers[y]);
-              this.registers[x] += this.registers[y];
               // check for overflow
-              if ((ans & 0xFF) === ans) {
+              if (ans > 255) {
                 this.registers[FLAG_REGISTER] = 0;
               } else {
                 this.registers[FLAG_REGISTER] = 1;
               }
+              this.registers[x] = ans & 0xFF;
               this.pc += 2;
               break;
             case 5:
@@ -256,16 +293,44 @@ class CPU {
           this.pc += 2;
           break;
         case 0xD000: // draw sprite with height n at x, y
-          x = (opcode & 0x0F00) >> 8;
-          y = (opcode & 0x00F0) >> 4;
-          n = opcode & 0x000F;
+          x = this.registers[(opcode & 0x0F00) >> 8] % WIDTH;
+          y = this.registers[(opcode & 0x00F0) >> 4] % HEIGHT;
+          height = opcode & 0x000F;
+
+          let row, col, byte, pixel;
+
+
+          this.registers[FLAG_REGISTER] = 0;
+
+
+          // update pixels
+          for (row = 0; row < height; ++row) {
+            byte = this.memory[this.I + row]
+            for (col = 0; col < SPRITE_WIDTH; ++col) {
+
+              pixel = this.display[(y + row) * WIDTH + (x + col)];
+              // sprite pixel is on
+              if ((byte & (0x80 >> col)) !== 0) {
+                // check for collision
+                if (pixel === ON) {
+                  this.registers[FLAG_REGISTER] = 1;
+                }
+                console.log((y + row) * WIDTH + (x + col))
+                this.display[(y + row) * WIDTH + (x + col)] ^= ON;
+              }
+
+            }
+          }
+          console.log(this.display)
+
           this.ctx.fillStyle = 'white'
           this.ctx.fillRect(
-            this.registers[x],
-            this.registers[y],
-            SPRITE_WIDTH,
-            n
+            x,
+            y,
+            1,
+            height
           )
+          // this.draw(x, y, n)
           this.pc += 2;
           break;
         case 0xE000:
@@ -280,7 +345,6 @@ class CPU {
               break;
             default:
               console.log("unrecognized opcode");
-              alert("unrecognized opcode")
               break
           }
           break
@@ -314,31 +378,29 @@ class CPU {
               break;
             case 0x29:
               x = (opcode & 0x0F00) >> 8;
-              this.I = sprite_addr[this.registers[x]];
+              this.I = FONTS_START + (FONT_LEN * this.registers[x]);
               this.pc += 2;
               break;
             case 0x33: // store binary-coded decimal representation of Vx
               x = (opcode & 0x0F00) >> 8;
-              this.I = Math.floor(x / 100); // set 100s place
-              this.I++;
-              this.I = Math.floor((x % 100) / 10) // set 10s place
-              this.I++;
-              this.I = x % 10; // set one's place
+              let val = this.registers[x];
+              for (let offset = 2; offset >= 0; offset--) {
+                this.memory[this.I + offset] = val % 10;
+                val = Math.floor(val / 10);
+              }
               this.pc += 2;
               break;
             case 0x55: // dump v0 to vx in memory starting from I
               x = (opcode & 0x0F00) >> 8;
-              addr = this.I;
-              for (let i = 0; i <= x; i++) {
-                this.memory[addr++] = this.registers[i];
+              for (let i = 0; i <= this.registers[x]; i++) {
+                this.memory[this.I + i] = this.registers[i];
               }
               this.pc += 2;
               break;
             case 0x65:// load memory starting from I into v0 to vx
               x = (opcode & 0x0F00) >> 8;
-              addr = this.I;
-              for (let i = 0; i <= x; i++) {
-                this.registers[i] = this.memory[addr++];
+              for (let i = 0; i <= this.registers[x]; i++) {
+                this.registers[i] = this.memory[this.I + i];
               }
               this.pc += 2;
               break;
@@ -346,7 +408,6 @@ class CPU {
           break;
         default:
           console.log(`unrecognized opcode: ${opcode}`)
-          alert("unrecognized opcode")
           break;
       }
       if (this.delay_timer > 0) {
@@ -365,11 +426,16 @@ class CPU {
 
 
   reset() {
+
+
     this.clear_display();
     if (this.interval !== null) {
       clearInterval(this.interval);
     }
-    this.memory = new Uint8Array(MEMORY_LOCATIONS);
+    // this.memory = new Uint8Array(MEMORY_SIZE);
+    for (let i = PROGRAM_START; i <= MEMORY_SIZE; i++) {
+      this.memory[i] = 0;
+    }
     this.stack = new Uint16Array(16);
 
     this.registers = new Uint8Array(NUM_REGISTERS);
@@ -386,7 +452,9 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   window.addEventListener("keydown", function (event) {
     keyPressed = INPUT_MAP[event.key];
-    console.log(keyPressed)
+  })
+  window.addEventListener("keyup", function (event) {
+    keyPressed = null;
   })
   const fileInput = document.getElementById("romFileInput");
   const resetBtn = document.getElementById("resetBtn");
